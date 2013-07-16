c30876349a9f3e8b66ba4c2d2baa25be4d7890b5
John Resig <jeresig@gmail.com>
2013-07-16 09:42:58 -0700

    Re-commit the HTML link changes and the improvement provided by @michaelficarra in #3.

--- structured.js
+++ structured.js
@@ -132,20 +132,13 @@ if (typeof module !== "undefined" && module.exports) {
      *        relative ordering matter).
      */
     function parseStructure(structure, wVars) {
-        // Prepend with an assignment so that function() {} becomes
-        //  valid standalone Javascript.
-        var fullTree = esprima.parse("structure = " + structure.toString());
-        try {
-            if (fullTree.body[0].expression.right.type !== "FunctionExpression" ||
-                !fullTree.body[0].expression.right.body) {
-                throw "Poorly formatted structure code";
-            }
-        } catch (e) {
-            // Also catches cases where the accesses in the if-statement
-            //  fail since fullTree is poorly formed.
-            throw "Poorly formatted structure code.";
+        // Wrapped in parentheses so function() {} becomes valid Javascript.
+        var fullTree = esprima.parse("(" + structure + ")");
+        if (fullTree.body[0].expression.type !== "FunctionExpression" ||
+            !fullTree.body[0].expression.body) {
+            throw "Poorly formatted structure code";
         }
-        var tree = fullTree.body[0].expression.right.body;
+        var tree = fullTree.body[0].expression.body;
         simplifyTree(tree, wVars);
         return tree;
     }
